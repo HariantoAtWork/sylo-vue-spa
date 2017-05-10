e9c9b400df9cac4b03f172f654690f7a224e9059
Harianto van Insulinde <hariantoatwork@gmail.com>
2017-05-11 00:48:33 +0200

    Move webpack-notfier plugin to base configuration

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var WebpackNotifierPlugin = require('webpack-notifier')
 
 function resolve (dir) {
 	return path.join(__dirname, '..', dir)
@@ -67,5 +68,8 @@ module.exports = {
 				name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
 			}
 		}]
-	}
+	},
+	plugins: [
+		new WebpackNotifierPlugin({ alwaysNotify: true })
+	]
 }
